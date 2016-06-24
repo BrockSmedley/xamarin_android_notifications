ffa9773111b5dfc99b475464879a63d4ac53ee92
Brock Smedley <brocksmedley@gmail.com>
2016-06-24 01:18:07 -0700

    Notification cancels self after being tapped

--- Notifications/Notifications/MainActivity.cs
+++ Notifications/Notifications/MainActivity.cs
@@ -23,55 +23,46 @@ namespace Notifications {
 
 			button.Click += delegate { 
 				// notify user
-				//notifyMe();
-
-				//create the notification object
-				Notification.Builder builder = new Notification.Builder(this);
-
-				//set vibrate pattern
-				long[] vibratePattern = { 30,100,30,100 };
-
-				//set the notification info
-				builder
-					.SetContentTitle("Notification")
-					.SetContentText("Here's your notification!")
-					.SetSmallIcon(Resource.Drawable.Icon)
-					.SetVibrate(vibratePattern)
-					.SetStyle(new Notification.BigTextStyle())
-					.SetCategory(Notification.CategoryReminder)
-					.SetPriority((int)NotificationPriority.High);
-				
-				
-
-				//get notification manager
-				NotificationManager manager = GetSystemService(Context.NotificationService) as NotificationManager;
-				
-				//build notification
-				Notification notification = builder.Build();
-				
-
-				//notify user
-				const int notificationId = 0;
-				manager.Notify(notificationId, notification);
+				notifyMe();				
 			};
 		}
 
 		private void notifyMe() {
+			//create intent to allow notification to bring user to app
+			Intent intent = new Intent(this, typeof(MainActivity));
+
+			//create PendingIntent because that's what notifications need
+			const int pendingIntentId = 0;
+			PendingIntent pendingIntent = PendingIntent.GetActivity(this, pendingIntentId, intent, PendingIntentFlags.OneShot);//OneShot: only run once
+
+
 			//create the notification object
 			Notification.Builder builder = new Notification.Builder(this);
 
+			//set vibrate pattern
+			long[] vibratePattern = { 30, 100, 30, 100 };
+
 			//set the notification info
-			builder.SetContentTitle("Notification");
-			builder.SetContentText("Here's your notification!");
+			builder
+				.SetContentTitle("Notification")
+				.SetContentText("Here's your notification!")
+				.SetSmallIcon(Resource.Drawable.Icon)
+				.SetVibrate(vibratePattern)
+				.SetStyle(new Notification.BigTextStyle())
+				.SetCategory(Notification.CategoryReminder)
+				.SetPriority((int)NotificationPriority.High)
+				.SetContentIntent(pendingIntent)
+				.SetAutoCancel(true);
+
 
 
 			//get notification manager
 			NotificationManager manager = GetSystemService(Context.NotificationService) as NotificationManager;
-			
-			
+
 			//build notification
-			Notification notification = builder.Build();			
-			
+			Notification notification = builder.Build();
+
+
 			//notify user
 			const int notificationId = 0;
 			manager.Notify(notificationId, notification);
