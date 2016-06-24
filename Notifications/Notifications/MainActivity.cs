e3f387eca0bece3b0a6ae245e705bd582af562d4
Brock Smedley <brocksmedley@gmail.com>
2016-06-23 23:32:45 -0700

    Notifies user on button press

--- Notifications/Notifications/MainActivity.cs
+++ Notifications/Notifications/MainActivity.cs
@@ -6,10 +6,10 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+
 namespace Notifications {
 	[Activity(Label = "Notifications", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity {
-		int count = 1;
 
 		protected override void OnCreate(Bundle bundle) {
 			base.OnCreate(bundle);
@@ -21,7 +21,55 @@ namespace Notifications {
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			button.Click += delegate { 
+				// notify user
+				//notifyMe();
+
+				//create the notification object
+				Notification.Builder builder = new Notification.Builder(this);
+
+				//set vibrate pattern
+				long[] vibratePattern = { 30,100,30,100 };
+
+				//set the notification info
+				builder
+					.SetContentTitle("Notification")
+					.SetContentText("Here's your notification!")
+					.SetSmallIcon(Resource.Drawable.Icon)
+					.SetVibrate(vibratePattern);
+				
+
+				//get notification manager
+				NotificationManager manager = GetSystemService(Context.NotificationService) as NotificationManager;
+				
+				//build notification
+				Notification notification = builder.Build();
+
+				//notify user
+				const int notificationId = 0;
+				manager.Notify(notificationId, notification);
+			};
+		}
+
+		private void notifyMe() {
+			//create the notification object
+			Notification.Builder builder = new Notification.Builder(this);
+
+			//set the notification info
+			builder.SetContentTitle("Notification");
+			builder.SetContentText("Here's your notification!");
+
+
+			//get notification manager
+			NotificationManager manager = GetSystemService(Context.NotificationService) as NotificationManager;
+			
+			
+			//build notification
+			Notification notification = builder.Build();			
+			
+			//notify user
+			const int notificationId = 0;
+			manager.Notify(notificationId, notification);
 		}
 	}
 }
